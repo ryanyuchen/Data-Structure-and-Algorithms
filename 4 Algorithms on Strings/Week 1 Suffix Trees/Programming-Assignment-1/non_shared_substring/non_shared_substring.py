a572d15923362610d314ac71d2f4135eb3734489
Ryan <vanchen10@gmail.com>
2019-09-06 14:37:40 -0400

    Add files via upload

--- 4 Algorithms on Strings/Week 1 Suffix Trees/Programming-Assignment-1/non_shared_substring/non_shared_substring.py
+++ 4 Algorithms on Strings/Week 1 Suffix Trees/Programming-Assignment-1/non_shared_substring/non_shared_substring.py
@@ -1,9 +1,51 @@
 # python3
 import sys
 
+def build_trie(patterns):
+    tree = dict()
+    # write your code here  
+    tree[0] = {}
+    index = 1
+    
+    for pattern in patterns:
+        current = tree[0]
+        '''
+        In Python, = is the shallow copy.
+        https://realpython.com/copying-python-objects/
+        '''
+        for c in pattern:
+            if c in current.keys():
+                current = tree[current[c]]
+            else:
+                current[c] = index
+                tree[index] = {}
+                current = tree[index]
+                index = index + 1
+        current['$'] = {}    
+    return tree
+
+
 def solve (p, q):
-	result = p
-	return result
+    patterns = [p, q]
+    for i in range(1, len(p)):
+        patterns.append(p[i:])
+        patterns.append(q[i:])
+        
+    trie = build_trie(patterns)
+    
+    current = trie[0]
+    substring = ""
+    
+    while '$' not in current or current == trie[0]:
+        for child in current:
+            substring += child
+            if child not in q:
+                return substring
+            else:
+                current = trie[current[child]]
+                substring = substring[:-1]
+                
+    return p
 
 p = sys.stdin.readline ().strip ()
 q = sys.stdin.readline ().strip ()
--- 4 Algorithms on Strings/Week 1 Suffix Trees/Programming-Assignment-1/suffix_tree/suffix_tree.py
+++ 4 Algorithms on Strings/Week 1 Suffix Trees/Programming-Assignment-1/suffix_tree/suffix_tree.py
@@ -1,6 +1,24 @@
 # python3
 import sys
 
+_END = '$'
+
+def compress_recursive(tree, node):
+    for (key, child_node) in tree[node].items():
+        if key == "start":
+            return
+        if len(tree[child_node]) == 1:
+            path_char = next(iter(tree[child_node]))
+            
+            if path_char != "start":
+                grand_child_node = tree[child_node][path_char]
+                tree[node][key + path_char] = grand_child_node
+                del tree[node][key]
+                del tree[child_node]
+                compress_recursive(tree, grand_child_node)
+                
+        else:
+            compress_recursive(tree, child_node)
 
 def build_suffix_tree(text):
   """
@@ -10,6 +28,33 @@ def build_suffix_tree(text):
   """
   result = []
   # Implement this function yourself
+  tree = dict()
+  tree[0] = {}
+  index = 0
+  
+  for start in range(len(text)):
+      word = text[start:]
+      current_node = 0
+      for char in word:
+          if char in tree[current_node].keys():
+              current_node = tree[current_node][char]
+          else:
+              index += 1
+              new_node = index
+              if char == _END:
+                  tree[new_node] = {"start": start}
+              else:
+                  tree[new_node] = {}
+                  tree[current_node][char] = new_node
+                  current_node = new_node
+  
+  current_node = 0
+  compress_recursive(tree, current_node)
+  for node in tree:
+      for c in tree[node]:
+          if c != "start":
+              result.append(c)
+      
   return result
 
 
